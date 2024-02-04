f319972158ab75448cfe9d0030e7a77750600550
Ivan <ivansanchezdiaz2001@gmail.com>
2024-02-03 21:27:00 -0500

    feat: basic window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use log::debug;
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{EventLoop, EventLoopWindowTarget},
+    window::{Theme, Window, WindowBuilder},
+};
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    let event_loop = EventLoop::new().unwrap();
+
+    let mut w = WindowBuilder::new()
+        .with_theme(Some(Theme::Dark))
+        .build(&event_loop)
+        .unwrap();
+
+    event_loop.run(|e, elwt| handler(&mut w, e, elwt)).unwrap();
+}
+
+fn handler(window: &mut Window, event: Event<()>, elwt: &EventLoopWindowTarget<()>) {
+    debug!("{:?}", event);
+    match event {
+        Event::WindowEvent { window_id, event } => {
+            if window_id != window.id() {
+                return;
+            }
+
+            match event {
+                WindowEvent::CloseRequested => elwt.exit(),
+                _ => {}
+            }
+        }
+        _ => {}
+    }
 }
